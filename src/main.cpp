c595b9c83bb27d6bba9e3cbec0a5a6400d8232ce
Pavel Kulyov <kulyov.pavel@gmail.com>
2022-12-18 21:13:53 +0300

    Yet another legacy opengl triangle, yay

---
+++ src/main.cpp
@@ -0,0 +1,83 @@
+#define GL_SILENCE_DEPRECATION
+
+#include <iostream>
+
+#include <argparse/argparse.hpp>
+#include <GL/glew.h>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+
+argparse::ArgumentParser parse_args(int argc, char** argv) {
+    argparse::ArgumentParser program("ogl-sandbox");
+
+    program.add_argument("square")
+        .help("number to calculate square")
+        .scan<'i', int>();
+
+    try {
+        program.parse_args(argc, argv);
+    }
+    catch (const std::runtime_error& err) {
+        std::cerr << err.what() << std::endl;
+        std::cerr << program;
+        std::exit(1);
+    }
+
+    return program;
+}
+
+
+int main(int argc, char** argv) {
+    auto args = parse_args(argc, argv);
+    auto input = args.get<int>("square");
+    std::cout << (input * input) << std::endl;
+
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "ogl-sandbox", NULL, NULL);
+    if (!window)
+    {
+        std::cout << "Error: Unable to create GLFW window." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK) {
+        std::cout << "Error: Unable to initialize GLEW." << std::endl;
+        glfwTerminate();
+        return -1;
+    };
+
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5, 0.0);
+        glVertex2f(0.0, 0.5);
+        glVertex2f(0.5, 0.0);
+        glEnd();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
