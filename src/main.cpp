c59734063c850fff64958f72b1c7ae9a034ff483
Pavel Kulyov <kulyov.pavel@gmail.com>
2022-12-19 16:15:44 +0300

    Make rectangle red using modern OpenGL monstrosity

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,45 @@ argparse::ArgumentParser parse_args(int argc, char** argv) {
 }
 
 
+static unsigned int compileShader(unsigned int type, const std::string& source) {
+    GLuint id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE) {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length,  message);
+        std::cout << "Failed to compile "
+                  << (type == GL_VERTEX_SHADER ? "vertex" : "fragment")
+                  << "shader: " << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+static GLuint createShader(const std::string& vertexShader, const std::string& fragmentShader) {
+    GLuint program = glCreateProgram();
+    GLuint vs = compileShader(GL_VERTEX_SHADER, vertexShader);
+    GLuint fs = compileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
+
 int main(int argc, char** argv) {
     auto args = parse_args(argc, argv);
     auto input = args.get<int>("square");
@@ -65,17 +104,52 @@ int main(int argc, char** argv) {
 
     std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
 
+    float dots[6] = {
+        -0.5, -0.5,
+         0.0,  0.5,
+         0.5, -0.5,
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+
+    unsigned int vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), dots, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+    glEnableVertexAttribArray(0);
+
+    std::string vertexShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) in vec4 position;\n"
+        "\n"
+        "void main() {\n"
+        "    gl_Position = position;\n"
+        "}\n";
+
+    std::string fragmentShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) out vec4 color;\n"
+        "\n"
+        "void main() {\n"
+        "    color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "}\n";
+
+    GLuint shader = createShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5, 0.0);
-        glVertex2f(0.0, 0.5);
-        glVertex2f(0.5, 0.0);
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
